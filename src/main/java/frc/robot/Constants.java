414a13e3952386722cc78fa96e00ee423f588a2e
RogerMCT <rogermct@hotmail.com>
2022-01-21 10:07:11 -0500

    Removed Unused Bits

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,26 +7,15 @@ public final class Constants {
     public static final int kRightMotor1Port = 1;
     public static final int kRightMotor2Port = 4;
 
-    public static final int[] kLeftEncoderPorts = new int[] {0, 1};
-    public static final int[] kRightEncoderPorts = new int[] {2, 3};
-    public static final boolean kLeftEncoderReversed = false;
-    public static final boolean kRightEncoderReversed = true;
-
-    public static final int kEncoderCPR = 1024;
-    public static final double kWheelDiameterInches = 6;
-    public static final double kEncoderDistancePerPulse =
-        // Assumes the encoders are directly mounted on the wheel shafts
-        (kWheelDiameterInches * Math.PI) / (double) kEncoderCPR;
-
     public static final boolean kGyroReversed = false;
 
-    public static final double kStabilizationP = 1;
-    public static final double kStabilizationI = 0.5;
-    public static final double kStabilizationD = 0;
+    public static final double kStabilizationP = 0.1; //1;
+    public static final double kStabilizationI = 1e-4;//0.5;
+    public static final double kStabilizationD = 1;//0;
 
-    public static final double kTurnP = 1;
-    public static final double kTurnI = 0;
-    public static final double kTurnD = 0;
+    public static final double kTurnP = 0.1;//1;
+    public static final double kTurnI = 1e-4;//0;
+    public static final double kTurnD = 1;//0;
 
     public static final double kMaxTurnRateDegPerS = 100;
     public static final double kMaxTurnAccelerationDegPerSSquared = 300;
